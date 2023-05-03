80a678c9af8f7d486f1bfdceef5b9c1b00a65416
Zachary Mowatt <zamowatt@gmail.com>
2023-05-03 14:29:41 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(num1) {
+    if (num1 >= 42){
+        return num1 - 42
+    } else {
+        return 42 - num1
+    }
+}
+
+
+
+function distanceFromHqInFeet(num1){
+    return distanceFromHqInBlocks(num1) * 264
+}
+
+function distanceTravelledInFeet(start, destination){
+    if (start >= destination){
+        return (start - destination)*264
+    } else {
+        return (destination-start) * 264
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination)
+    if (distance <= 400){
+        return 0
+    } else if (distance <= 2000){
+        return (distance - 400) * .02
+    } else if (distance <= 2500){
+        return 25
+    } else if (distance > 2500){
+        return 'cannot travel that far'
+    }
+}
