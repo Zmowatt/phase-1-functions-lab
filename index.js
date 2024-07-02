3b8aae02549c9c31edf3aad8d4ab75b5c2c2dace
Zachary Mowatt <zamowatt@gmail.com>
2024-07-01 22:10:58 -0400

    complete lab

--- index.js
+++ index.js
@@ -32,3 +32,38 @@ function calculatesFarePrice(start, destination){
         return 'cannot travel that far'
     }
 }
+
+
+
+// function distanceFromHqInBlocks(pickUpLocation){
+//     if(pickUpLocation >= 42){
+//         return pickUpLocation - 42
+//     } else if(pickUpLocation <= 42){
+//         return 42 - pickUpLocation
+//     }
+// }
+
+// function distanceFromHqInFeet(pickUpLocation){
+//     return distanceFromHqInBlocks(pickUpLocation)*264
+// }
+
+// function distanceTravelledInFeet(start, destination){
+//     if(start >= destination){
+//         return (start - destination) * 264
+//     } else if(start < destination){
+//         return (destination - start) * 264
+//     }
+// }  
+
+// function calculatesFarePrice(start, destination){   
+//     const distance = distanceTravelledInFeet(start, destination);   
+//     if(distance <= 400){
+//         return 'Your ride is on us! It is Free!'
+//     } else if(distance <= 2000){
+//         return ((distance - 400) *.02)
+//     } else if(distance <= 2500){
+//         return 25
+//     } else if(distance > 2500){
+//         return 'cannont travel that far'
+//     }
+// }
